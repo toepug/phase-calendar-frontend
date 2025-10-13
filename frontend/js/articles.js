2048acef8f15935622abb6d7dba1a63c2641fd12
toepug <153052989+toepug@users.noreply.github.com>
2025-10-13 10:08:13 -0400

    Add files via upload

--- frontend/js/articles.js
+++ frontend/js/articles.js
@@ -2,29 +2,29 @@
 
 const articles = [
     {
+        title: "The Confusing History of the Names of the Months",
+        date: "October 16, 2025",
+        description: "Uncover the messy origins of the names of the months, from Roman gods to emperors, and see how The Phase Calendar offers a logical alternative.",
+        url: "articles/the-confusing-history-of-the-names-of-the-months.html"
+    },
+    {
+        title: "The Ancient Origins of the Days of the Week Names",
+        date: "October 13, 2025",
+        description: "Explore the fascinating history of the days of the week names, from Roman gods to Norse myths, and discover how The Phase Calendar simplifies scheduling.",
+        url: "articles/the-ancient-origins-of-the-days-of-the-week-names.html"
+    },
+    {
+        title: "The 4-Day Work Week & The Calendar Problem",
+        date: "October 10, 2025",
+        description: "Discover how the 4-day work week boosts productivity and well-being, and why the Phase Calendar’s 6-day row solves scheduling woes. Try it today!",
+        url: "articles/the-4-day-work-week.html"
+    },
+    {
         title: "Why Is Our Calendar So Weird? The Case for a More Logical Year",
         date: "October 7, 2025",
         description: "A journey through the messy history of our calendar and a look at a modern, logical alternative.",
         url: "articles/why-our-calendar-is-weird.html"
     }
-    {
-        title: "The 4-Day Work Week & The Calendar Problem",
-        date: "October 10, 2025",
-        description: "Discover how the 4-day work week boosts productivity and well-being, and why the Phase Calendar’s 6-day row solves scheduling woes. Try it today!",
-        url: "the-4-day-work-week.html"
-    }
-    {
-        title: "The Ancient Origins of the Days of the Week Names",
-        date: "October 13, 2025",
-        description: "Explore the fascinating history of the days of the week names, from Roman gods to Norse myths, and discover how The Phase Calendar simplifies scheduling.",
-        url: "the-ancient-origins-of-the-days-of-the-week-names.html"
-    }
-     {
-        title: "The Confusing History of the Names of the Months",
-        date: "October 16, 2025",
-        description: "Uncover the messy origins of the names of the months, from Roman gods to emperors, and see how The Phase Calendar offers a logical alternative.",
-        url: "the-confusing-history-of-the-names-of-the-months.html"
-    }
-    // When you write a new article, you'll add another object here.
 ];
+
 
